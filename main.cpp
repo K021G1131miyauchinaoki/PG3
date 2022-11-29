bc1a683c12a90985164344fcf8d6413ba9e6faf8
k021g1131@g.neec.ac.jp <mk021g1131@g.neec.ac.jp>
2022-11-30 00:33:01 +0900

    単方向リスト作成

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 typedef	struct cell
 {
 	int	val;
-	struct Cell* next;
+	struct cell* next;
 }CELL;
 //セルを新規作成する関数
 void	Create(CELL* head_, int val_) {
@@ -17,10 +17,36 @@ void	Create(CELL* head_, int val_) {
 
 	nextCell->val = val_;
 	nextCell->next = nullptr;
+
+	while (head_->next != nullptr)
+	{
+		head_ = head_->next;
+	}
+	head_->next = nextCell;
+}
+
+void	index(CELL*head_){
+	while (head_->next != nullptr)
+	{
+		head_ = head_->next;
+		printf("%d\n", head_->val);
+	}
 }
 
 int	main() {
+	int	val;
 	//先頭のセルを宣言
 	CELL	head;
 	head.next = nullptr;
+	while (true)
+	{
+		printf("値を入力してください\n");
+		scanf_s("%d", &val);
+		printf("入力した値を表示\n");
+		//最後尾にセルを追加
+		Create(&head, val);
+		//リスト一覧の表示
+		index(&head);
+	}
+	return	0;
 }
