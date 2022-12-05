2029bbf565ddbafeaadea72c5c19aea277f41e48
k021g1131@g.neec.ac.jp <mk021g1131@g.neec.ac.jp>
2022-12-05 11:30:39 +0900

    単方向リスト 氏名、貸出日、返却日を入力できるようにした

--- main.cpp
+++ main.cpp
@@ -4,16 +4,21 @@
 
 typedef	struct cell
 {
-	char str[256];
+	char name[256];
+	int checkoutDate;
+	int returnDate;
 	struct cell* next;
 }CELL;
 //セルを新規作成する関数
-void	Create(CELL* head_, const char* str_) {
+void	Create(CELL* head_, const char* name_,int checkoutDate_,int returnDate_) {
 	CELL* newCell;
 	//新規作成するセルのポインタ
 	newCell = (CELL*)malloc(sizeof(CELL));
 
-	strcpy_s(newCell->str, 256, str_);
+	strcpy_s(newCell->name, 256, name_);
+	newCell->checkoutDate = checkoutDate_;
+	newCell->returnDate = returnDate_;
+
 	newCell->next = nullptr;
 
 	//nullptrのポインタまで飛ぶ
@@ -28,23 +33,32 @@ void	index(CELL* head_) {
 	while (head_->next != nullptr)
 	{
 		head_ = head_->next;
-		printf("%s\n", head_->str);
+		printf("%s\n", head_->name);
+		printf("%d\n", head_->checkoutDate);
+		printf("%d\n", head_->returnDate);
+		printf("\n");
 	}
 }
 
 int	main() {
-	char val[256];
+	char name[256];
+	int checkoutDate;
+	int returnDate;
 	//先頭のセルを宣言
 	CELL	head;
 	head.next = nullptr;
 	while (true)
 	{
-		printf("値を入力してください\n");
-		scanf_s("%s", val,256);
-		printf("入力した値を表示\n");
 		//最後尾にセルを追加
-		Create(&head, val);
+		printf("名前を入力してください\n");
+		scanf_s("%s", name,256);
+		printf("貸出日を入力してください\n");
+		scanf_s("%d", &checkoutDate);
+		printf("返却日を入力してください\n");
+		scanf_s("%d", &returnDate);
+		Create(&head, name,checkoutDate,returnDate);
 		//リスト一覧の表示
+		printf("入力した値を表示\n");
 		index(&head);
 	}
 	system("pause");
