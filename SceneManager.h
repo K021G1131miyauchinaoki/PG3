744f4d39c5361bc27e740580702b6d4a94b05bf8
k021g1131@g.neec.ac.jp <mk021g1131@g.neec.ac.jp>
2023-01-22 16:43:36 +0900

    シングルトンパターンでシーン管理

---
+++ SceneManager.h
@@ -0,0 +1,15 @@
+#pragma once
+class SceneManager
+{
+private:
+	SceneManager();
+	~SceneManager();
+public:
+	SceneManager(const SceneManager& obj) = delete;
+	SceneManager& operator=(const SceneManager& obj) = delete;
+
+	static SceneManager* GetInstance();
+
+	static void ChangeScene(int sceneNo);
+};
+
--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
 #include"Test.h"
+#include"SceneManager.h"
 
 int	main() {
+	int sceneNo = 0;
+
+	while (true)
+	{
+		system("cls");
+		SceneManager::ChangeScene(sceneNo);
+		switch (_getch())
+		{
+		case 0x20:
+			sceneNo++;
+			if (sceneNo==4)
+			{
+				sceneNo = 0;
+			}
+			break;
+		default:
+			break;
+		}
+
+	}
 	//Test::GetInstance();
-	Test* test = Test::GetInstance();
+	//Test* test = Test::GetInstance();
 	//printf("1");
+
+	system("Pause");
+
 	return	0;
 }
