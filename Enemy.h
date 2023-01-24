e2636530b20b6d558e1031eeba7e04213ea0510f
k021g1131@g.neec.ac.jp <mk021g1131@g.neec.ac.jp>
2023-01-25 00:25:55 +0900

    静的メンバ変数課題完了

---
+++ Enemy.h
@@ -0,0 +1,21 @@
+#pragma once
+#include<stdio.h>
+
+class Enemy
+{
+protected:
+public:
+	Enemy() {
+		num++;
+		printf("“G%d‚ªŒ»‚ê‚½\n", num);
+	}
+	~Enemy() {
+		num--;
+	}
+	static void AllFallDown();
+	static int num;
+	static bool isDead;
+private:
+};
+
+
